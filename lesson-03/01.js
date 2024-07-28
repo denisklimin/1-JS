140663c21cc3564e5f2c8d7e8a980e0002e00cad
denisklimin <denisbusinessman1995@gmail.com>
2024-07-28 13:38:45 +0200

    l3_1

---
+++ lesson-03/01.js
@@ -0,0 +1,11 @@
+// Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+let first = 5;
+let second = 10;
+
+function calculateAverage(a, b) {
+    let c = 0;
+    c = (a + b) / 2;
+    console.log(c);
+}
+
+calculateAverage(10, 15);
