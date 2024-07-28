c06902b7ea292a22fbf2b2e292d261f17d297d32
denisklimin <denisbusinessman1995@gmail.com>
2024-07-28 14:15:02 +0200

    l3_3_bf_1

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,11 +1,13 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(a, b, c) {
-    console.log(Math.max(a,b,c));
+    return Math.max(a,b,c);
 }
 
 const num1 = +prompt('First');
 const num2 = +prompt('Second');
 const num3 = +prompt('Trird');
 
-findLargest(num1, num2, num3);
+const max = findLargest(num1, num2, num3);
+console.log(max);
+alert(max);
